2f183edf65b1b8435c2cac3115673b7c25ab5967
fmandaya <andayaf@student.douglascollege.ca>
2023-11-27 08:04:35 -0800

    Florence: started the daily exercise activity and layout

---
+++ app/src/main/java/com/example/fitlifepro/ExerciseDayActivity.java
@@ -0,0 +1,14 @@
+package com.example.fitlifepro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_day);
+    }
+}
